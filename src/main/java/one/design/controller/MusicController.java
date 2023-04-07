f394b539002475a75071c0ac6fe694ed505f4729
Ludeng0727 <97971814+Ludeng0727@users.noreply.github.com>
2023-04-07 20:50:30 +0900

    FEAT MusicController MusicInterceptor

--- src/main/java/one/design/controller/MusicController.java
+++ src/main/java/one/design/controller/MusicController.java
@@ -1,10 +1,16 @@
 package one.design.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MusicController {
 
+    @GetMapping("/getMusic")
+    public void getMusic(){
+        //MidiController 에 요청
 
+
+    }
 
 }
